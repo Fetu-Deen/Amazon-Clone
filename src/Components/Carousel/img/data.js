775cef73345a9d5ee9eabeb75afd1fe6d5494537
Fetu Deen <fettabin@gmail.com>
2024-09-18 16:51:55 +0300

    Category section

---
+++ src/Components/Carousel/img/data.js
@@ -0,0 +1,8 @@
+import img1 from "./0d4c6c79-e5a3-480e-b4e8-4c1cb0069133.jpeg";
+import img2 from "./2533ffcc-ca6a-4afb-84f9-cfc8f4dbc317.jpeg";
+import img3 from "./373a3f62-ef8d-4679-95b3-1b349bc46d9e.jpeg";
+import img4 from "./3b285d7e-60c8-4e08-bcc0-69ebf38af554.jpeg";
+import img5 from "./523524ab-6015-454e-a89e-4475a2d4fd57.jpeg";
+
+const img = [img1, img2, img3, img4, img5];
+export default img;
---
+++ src/Components/Category/categoryFullInfos.js
@@ -0,0 +1,30 @@
+// let data = [https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_TV_2X._SY304_CB432517900_.jpg
+// ,https://m.media-amazon.com/images/I/91Zu+hBrNOL._AC_SX569_.jpg
+// , https://m.media-amazon.com/images/I/618bcm65ksL._AC_UL480_FMwebp_QL65_.jpg
+// , https://m.media-amazon.com/images/I/71r7eWuCsaL._AC_UL480_FMwebp_QL65_.jpg ]
+
+export const categoryInfos = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imageLink:
+      "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_TV_2X._SY304_CB432517900_.jpg",
+  },
+  {
+    title: "Discover Fashion Trends",
+    name: "women's clothing",
+    imageLink: "https://m.media-amazon.com/images/I/91Zu+hBrNOL._AC_SX569_.jpg",
+  },
+  {
+    title: "Men's clothing",
+    name: "men's clothing",
+    imageLink:
+      "https://m.media-amazon.com/images/I/618bcm65ksL._AC_UL480_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "Jewelry",
+    name: "Jewelry",
+    imageLink:
+      "https://m.media-amazon.com/images/I/71r7eWuCsaL._AC_UL480_FMwebp_QL65_.jpg",
+  },
+];
